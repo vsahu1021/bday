98a38c328edac8584da0017ae7b98fd80bc888d0
vsahu1021 <vsahu5764@gmail.com>
2020-06-07 16:44:55 +0530

    first

---
+++ main/script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Idiot Programmer"&&b=="subscribe")
+  	{
+  	    window.location.href="main/start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
