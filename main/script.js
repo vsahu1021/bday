44fe6840c40834653db1549796507abc9a428fdc
vsahu1021 <66550937+vsahu1021@users.noreply.github.com>
2020-06-07 16:55:37 +0530

    Update script.js

--- main/script.js
+++ main/script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a== "Idiot Programmer"&&b=="subscribe")
+  	if(a== "chadda"&&b=="june")
   	{
   	    window.location.href="main/start.html";
   	}
